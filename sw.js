b0cd8d174933223c56cc6732b1a8ff06a985d087
santinoRHG <santino@theriverhousegroup.com>
2026-06-22 20:24:09 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'tareas-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).then(res => {
+    if (res.ok) { const clone = res.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); }
+    return res;
+  }).catch(() => caches.match(e.request)));
+});
